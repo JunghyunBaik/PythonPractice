4d5c51054aff9e6f3c1dcc63206d1e309772e5e3
Junghyun <0430jenny37@gmail.com>
2021-07-02 10:35:49 +0900

    Answer of Q4&5

--- 5.py
+++ 5.py
@@ -1,3 +1,4 @@
+'''
 Question:
 Define a class which has at least two methods:
 getString: to get a string from console input
@@ -6,3 +7,17 @@ Also please include simple test function to test the class methods.
 
 Hints:
 Use __init__ method to construct some parameters
+'''
+class Methods:
+    def __init__(self):
+        self.result = ''
+
+    def getString(self):
+        self.result = input('your str : ')
+
+    def printString(self):
+        print(self.result.upper())
+
+method1 = Methods()
+method1.getString()
+method1.printString()
